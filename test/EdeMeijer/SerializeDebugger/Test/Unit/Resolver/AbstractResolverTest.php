cbd663ea9f0402f7aa4ffbd533ba2cc70e162909
Ede Meijer <ede.meijer@hotmail.com>
2014-09-02 08:22:25 +0200

    finished AbstractResolver unit test

--- test/EdeMeijer/SerializeDebugger/Test/Unit/Resolver/AbstractResolverTest.php
+++ test/EdeMeijer/SerializeDebugger/Test/Unit/Resolver/AbstractResolverTest.php
@@ -3,8 +3,9 @@
 namespace EdeMeijer\SerializeDebugger\Test\Unit\Resolver;
 
 use EdeMeijer\SerializeDebugger\Context;
-use EdeMeijer\SerializeDebugger\Node;
 use EdeMeijer\SerializeDebugger\Resolver\AbstractResolver;
+use EdeMeijer\SerializeDebugger\Type\PrimitiveType;
+use EdeMeijer\SerializeDebugger\Type\TypeInterface;
 use PHPUnit_Framework_MockObject_MockObject;
 
 class AbstractResolverTest extends \PHPUnit_Framework_TestCase
@@ -16,8 +17,38 @@ class AbstractResolverTest extends \PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->SUT = $this->getMockForAbstractClass('EdeMeijer\SerializeDebugger\Resolver\AbstractResolver');
+        $this->SUT = $this->getMockBuilder('EdeMeijer\SerializeDebugger\Resolver\AbstractResolver')
+            ->setMethods(['getProperties'])
+            ->getMockForAbstractClass();
+
         $this->context = new Context();
+    }
+
+    public function testItSetsTheNodeTypeIfSupported()
+    {
+        $type = new PrimitiveType();
+        $this->givenTypeSupportedReturns(true);
+        $this->givenGetTypeReturns($type);
+        $this->givenGetPropertiesReturns([]);
+        $node = $this->context->getNodeForData(null);
+
+        $this->SUT->resolve($node, $this->context);
+
+        $this->assertEquals($type, $node->getType());
+    }
+
+    public function testItAddsChildNodesBasedOnProperties()
+    {
+        $properties = ['a' => 1, 'b' => 2];
+        $this->givenTypeSupportedReturns(true);
+        $this->givenGetTypeReturns(new PrimitiveType());
+        $this->givenGetPropertiesReturns($properties);
+        $node = $this->context->getNodeForData(null);
+
+        $resolvedNode = $this->SUT->resolve($node, $this->context);
+
+        $this->assertCount(2, $resolvedNode->getChildNodes());
+        $this->assertCount(3, $this->context->getNodes());
     }
 
     /**
@@ -26,7 +57,8 @@ class AbstractResolverTest extends \PHPUnit_Framework_TestCase
     public function testItThrowsExceptionWhenTypeIsNotSupported()
     {
         $this->givenTypeSupportedReturns(false);
-        $node = new Node(0, true);
+        $node = $this->context->getNodeForData(null);
+
         $this->SUT->resolve($node, $this->context);
     }
 
@@ -38,6 +70,26 @@ class AbstractResolverTest extends \PHPUnit_Framework_TestCase
         $this->SUT->expects($this->any())
             ->method('supports')
             ->will($this->returnValue($supported));
+    }
+
+    /**
+     * @param TypeInterface $type
+     */
+    private function givenGetTypeReturns(TypeInterface $type)
+    {
+        $this->SUT->expects($this->any())
+            ->method('getType')
+            ->will($this->returnValue($type));
+    }
+
+    /**
+     * @param mixed[] $properties
+     */
+    private function givenGetPropertiesReturns(array $properties)
+    {
+        $this->SUT->expects($this->any())
+            ->method('getProperties')
+            ->will($this->returnValue($properties));
     }
 }
  
